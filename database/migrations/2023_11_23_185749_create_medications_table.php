5501673b3a97f19da65283ae80842f4cc52f94c1
brevis-ng <brevisnguyen@gmail.com>
2023-11-24 05:10:39 +0800

    Add model and migration

---
+++ database/migrations/2023_11_23_185749_create_medications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->decimal('price')->nullable();
+            $table->decimal('cost')->nullable();
+            $table->foreignId('unit_id')->constrained();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medications');
+    }
+};
