41e18c5cf6a9f9c82ee74d2120a04cba85353f94
brevis-ng <brevisnguyen@gmail.com>
2023-11-27 04:34:51 +0800

    Add medicines disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,13 @@ return [
             'throw' => false,
         ],
 
+        'medicines' => [
+            'driver' => 'local',
+            'root' => storage_path('app/medicines'),
+            'url' => env('APP_URL').'/medicines',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
     ],
 
     /*
@@ -71,6 +78,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('medicines') => storage_path('app/medicines'),
     ],
 
 ];
