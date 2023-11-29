1f02f9bd63ee22a5f39b793f8217dbad5a61ed00
brevis-ng <brevisnguyen@gmail.com>
2023-11-30 04:23:30 +0800

    Make pivot pivot, fix order&medicine relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -12,8 +13,13 @@ class Order extends Model
 
     protected $fillable = ['customer', 'phone', 'payment_method', 'total_amount'];
 
-    public function medications(): BelongsToMany
+    // public function medications(): BelongsToMany
+    // {
+    //     return $this->belongsToMany(Medication::class, 'order_medication')->withPivot('quantity');
+    // }
+
+    public function orderMedications(): HasMany
     {
-        return $this->belongsToMany(Medication::class, 'order_medication');
+        return $this->hasMany(OrderMedication::class);
     }
 }
