729b6963c83ff5c901eecf6af969ea51e651193f
brevis-ng <brevisnguyen@gmail.com>
2023-11-30 04:22:07 +0800

    Fix orders migration

--- database/migrations/2023_11_28_223401_create_orders_table.php
+++ database/migrations/2023_11_28_223401_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('customer');
-            $table->string('phone')->nullable;
+            $table->string('phone')->nullable();
             $table->string('payment_method');
             $table->string('total_amount');
             $table->timestamps();
