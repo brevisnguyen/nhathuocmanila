1e94ec12add17985414181677d6cdf18e159ffd9
brevis-ng <brevisnguyen@gmail.com>
2023-11-30 04:25:52 +0800

    Update order total amount field

--- database/migrations/2023_11_28_223401_create_orders_table.php
+++ database/migrations/2023_11_28_223401_create_orders_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('customer');
             $table->string('phone')->nullable();
             $table->string('payment_method');
-            $table->string('total_amount');
+            $table->decimal('total_amount');
             $table->timestamps();
         });
     }
