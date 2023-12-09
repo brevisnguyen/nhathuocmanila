a0552d7ca629150e58e3ab894ca03b13b37bb473
brevis-ng <brevisnguyen@gmail.com>
2023-12-09 14:54:02 +0800

    create homepage

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,27 @@
+<div class="bg-white text-slate-900">
+    <header>
+        <div class="header-top hidden md:block w-full bg-neutral-100 text-slate-800">
+            <div class="container py-3 flex flex-wrap justify-between">
+                <div class="my-auto grid grid-cols-2 divide-x">
+                    <a href="https://t.me/nhathuocmanila" class="inline-flex gap-x-1 justify-center items-center hover:text-lime-500">
+                        <i class="fa-brands fa-telegram relative"></i>
+                        <p class="my-auto">nhathuocmanila</p>
+                    </a>
+                    <p class="ps-4 text-sm my-auto">Tư vấn miễn phí 24/7</p>
+                </div>
+                <div class="social flex justify-center items-center gap-x-4">
+                    <a href="#" class="hover:text-blue-500"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="hover:text-red-500"><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="#" class="hover:text-lime-500"><i class="fa-brands fa-telegram"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="grid grid-cols-12 items-center">
+                <div class="logo col-span-3">1</div>
+                <div class="logo col-span-6">2</div>
+                <div class="logo col-span-3">3</div>
+            </div>
+        </div>
+    </header>
+</div>
