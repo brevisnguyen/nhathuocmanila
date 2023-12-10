a5cd49c0a3ead3bb76c8e7d2deeca208838a8a04
brevis-ng <brevisnguyen@gmail.com>
2023-12-10 15:17:01 +0800

    Install splide

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,15 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
+import Splide from '@splidejs/splide';
+
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+var splide = new Splide('.splide', {
+    type  : 'fade',
+    rewind: true,
+    autoplay: true,
+});
+
+splide.mount();
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
         extend: {},
         container: {
             center: true,
-            padding: '2rem',
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+            },
         }
     },
     plugins: [],
