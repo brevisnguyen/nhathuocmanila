99b828273f75e2dd0ebc17218f2164608b015a7a
brevis-ng <brevisnguyen@gmail.com>
2023-12-13 19:32:53 +0800

    Add posts resource

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+    protected static ?string $navigationGroup = 'Blogs';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationLabel = 'Bài viết';
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Thông tin bài viết')
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->label('Tiêu đề')
+                            ->required()
+                            ->maxLength(255)
+                            ->columnSpan(['default' => 1, 'sm' => 2, 'md' => 3])
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                        Forms\Components\TextInput::make('slug')
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->columnSpan(['default' => 1, 'sm' => 2, 'md' => 3]),
+                        Forms\Components\TextInput::make('views')
+                            ->label('Số lượt xem')
+                            ->disabled()
+                            ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2]),
+                        Forms\Components\FileUpload::make('featured_image')
+                            ->label('Ảnh bìa bài viết')
+                            ->image()
+                            ->required()
+                            ->openable()
+                            ->columnSpanFull()
+                            ->disk('posts')
+                            ->getUploadedFileNameForStorageUsing(
+                                fn (TemporaryUploadedFile $file): string => (string) str($file->getFilename())->prepend('featured-image-'),
+                            ),
+                    ])->columns([
+                        'default' => 1,
+                        'sm' => 5,
+                        'md' => 8,
+                    ]),
+                Forms\Components\Section::make('Nội dung')
+                    ->schema([
+                        Forms\Components\RichEditor::make('content')
+                            ->label('Nội dung bài viết')
+                            ->fileAttachmentsDisk('posts')
+                            ->required(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')->label('Tiêu đề'),
+                Tables\Columns\TextColumn::make('views')
+                    ->label('Lượt xem')
+                    ->sortable()
+                    ->default(0)
+                    ->icon('heroicon-s-eye')
+                    ->iconColor('success'),
+                Tables\Columns\ImageColumn::make('featured_image')
+                    ->label('Ảnh bìa')
+                    ->disk('posts'),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Ngày cập nhật')
+                    ->since(),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
