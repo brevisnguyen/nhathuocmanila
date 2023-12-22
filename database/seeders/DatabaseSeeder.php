57edb52ee89f425ceebc5611cf75badc38a5edab
brevis-ng <brevisnguyen@gmail.com>
2023-12-22 16:42:43 +0800

    Create model factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Medication;
+use App\Models\Post;
+use App\Models\Unit;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,14 +19,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::create([
+        User::create([
             'name' => 'Brevis',
             'password' => Hash::make('admin123'),
             'email' => 'brevisnguyen@example.com',
         ]);
 
-        \App\Models\Category::create(['name' => 'Khác', 'slug' => 'danh-muc-khac']);
-
-        \App\Models\Unit::create(['name' => 'Viên']);
+        Medication::factory()
+            ->has(Category::factory()->count(3))
+            ->for(Unit::factory())
+            ->count(10)
+            ->create();
+        Post::factory()
+            ->count(20)
+            ->create();
     }
 }
