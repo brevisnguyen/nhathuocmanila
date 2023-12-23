329131758b0b9bd832423696f3bfccb01eafd032
brevis-ng <brevisnguyen@gmail.com>
2023-12-23 11:18:47 +0800

    update post card

--- app/Livewire/PostCard.php
+++ app/Livewire/PostCard.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class PostCard extends Component
 {
+    public $post;
+
+    public function mount($post)
+    {
+        $this->post = $post;
+    }
+
     public function render()
     {
         return view('livewire.post-card');
--- resources/views/livewire/post-card.blade.php
+++ resources/views/livewire/post-card.blade.php
@@ -1,13 +1,13 @@
-<div class="post bg-white flex items-center overflow-hidden mb-4 md:mb-5">
-    <a class="img block h-[74px] flex-initial basis-[132px] md:h-[84px] md:basis-[144px]" href="#">
-        <img class="aspect-video object-cover !rounded-lg" src="{{ asset('posts/featured-image-WQnsWh2sbJJOEXJ7bSBadoDTCizzyT-metaQmlkaXZpdEFELnBuZw==-.png') }}" alt="">
+<div class="post bg-white flex items-center overflow-hidden mb-4 md:mb-5 hover:text-lime-500">
+    <a href="{{ $post->getUrl() }}" class="img block h-[74px] flex-initial basis-[132px] md:h-[84px] md:basis-[144px]">
+        <img class="w-full h-full object-cover !rounded-lg" src="{{ $post->getImage() }}" alt="{{ $post->title . '-' . config('app.name') }}">
     </a>
     <div class="ml-3 flex-1">
-        <a class="mb-[6px] block" href="#">
-            <h3 class="text-base md:text-lg font-bold my-2">Tiêu đề bài viết</h3>
+        <a class="mb-[6px] block" href="{{ $post->getUrl() }}">
+            <h3 class="text-base font-semibold mb-1 line-clamp-2 text-ellipsis">{{ $post->title }}</h3>
         </a>
-        <a class="block" href="#">
-            <p class="!text-sm line-clamp-2 text-ellipsis">Một phần nội dung được hiển thị ở đây, nó sẽ có 2 hàng và được cắt nếu quá dài</p>
+        <a class="block" href="{{ $post->getUrl() }}">
+            <p class="!text-sm line-clamp-2 text-ellipsis">{{ $post->content }}</p>
         </a>
     </div>
 </div>
