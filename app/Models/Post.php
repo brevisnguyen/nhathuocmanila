289aa6c885c862b4cad4d800dfe9a64165568e63
brevis-ng <brevisnguyen@gmail.com>
2023-12-23 11:19:05 +0800

    add function to model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'featured_image', 'content', 'views'];
+
+    public function getUrl(): string
+    {
+        return route('posts', ['post' => $this->slug]);
+    }
+
+    public function getImage(): string
+    {
+        if (Str::startsWith($this->featured_image, ['http://', 'https://', '//'])) {
+            return $this->featured_image;
+        } else {
+            return Storage::disk('medicines')->url($this->featured_image);
+        }
+    }
 }
