9b24f7c0cfb39ad7965a91175205fdfc93664d1d
brevis-ng <brevisnguyen@gmail.com>
2024-01-16 06:42:46 +0800

    update break-point

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,14 @@ export default {
         extend: {},
         container: {
             center: true,
-            padding: {
-                DEFAULT: '1rem',
-                sm: '2rem',
-                lg: '2rem',
-                xl: '2rem',
-            },
+            padding: '0.7rem',
         },
         screens: {
             'sm': '640px',
             'md': '768px',
-            'lg': '1170px',
-            'xl': '1170px',
-            '2xl': '1170px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1280px',
         },
         fontFamily: {
             'nunito': ['Nunito', 'sans-serif'],
