524cc9904955eaf9d0c79d6a698d4f7b9b66a327
brevis-ng <brevisnguyen@gmail.com>
2024-02-04 19:13:31 +0800

    refactor: footer

--- app/Livewire/Footer.php
+++ app/Livewire/Footer.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Settings\WebSettings;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Footer extends Component
 {
+    public Collection $website;
+
+    public function mount()
+    {
+        $this->website = (new WebSettings())->toCollection();
+    }
+
     public function render()
     {
         return view('livewire.footer');
--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -2,14 +2,14 @@
     <div class="container pb-3 md:pb-6 pt-6 md:pt-10">
         <div class="footer-top block lg:grid lg:grid-cols-2">
             <div class="logo w-full">
-                <img class="h-14" src="{{ asset('storage/logo-nha-thuoc-manila.png') }}" alt="">
+                <img class="h-14" src="{{ asset('storage/'.$website['logo']) }}" alt="">
                 <h4 class="slogan mt-5 md:mt-10">Chuyên thuốc theo toa, thuốc đặc trị, tư vấn miễn phí.</h4>
-                <h4 class="phone text-base text-indigo-500"><i class="fa-solid fa-square-phone"></i>&nbsp;<span class="font-bold">0985 435 9999</span></h4>
+                <h4 class="phone text-base text-indigo-500"><i class="fa-solid fa-square-phone"></i>&nbsp;<span class="font-bold">{{ $website['hotline'] }}</span></h4>
                 <h4 class="font-semibold text-base mt-4 mb-2">Tìm chúng tớ trên</h4>
                 <div class="socials flex gap-x-6 items-center">
-                    <a href="#" class="hover:text-white hover:bg-lime-500 h-10 w-10 flex items-center justify-center rounded-full border border-solid border-gray-200 bg-white text-gray-600"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" class="hover:text-white hover:bg-lime-500 h-10 w-10 flex items-center justify-center rounded-full border border-solid border-gray-200 bg-white text-gray-600"><i class="fa-brands fa-tiktok"></i></a>
-                    <a href="#" class="hover:text-white hover:bg-lime-500 h-10 w-10 flex items-center justify-center rounded-full border border-solid border-gray-200 bg-white text-gray-600"><i class="fa-solid fa-paper-plane"></i></a>
+                    <a href="{{ 'https://www.facebook.com/' . $website['facebook'] }}" class="hover:text-white hover:bg-lime-500 h-10 w-10 flex items-center justify-center rounded-full border border-solid border-gray-200 bg-white text-gray-600"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="{{ 'https://www.tiktok.com/@' . $website['tiktok'] }}" class="hover:text-white hover:bg-lime-500 h-10 w-10 flex items-center justify-center rounded-full border border-solid border-gray-200 bg-white text-gray-600"><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="{{ 'https://t.me/' . $website['telegram'] }}" class="hover:text-white hover:bg-lime-500 h-10 w-10 flex items-center justify-center rounded-full border border-solid border-gray-200 bg-white text-gray-600"><i class="fa-solid fa-paper-plane"></i></a>
                 </div>
             </div>
             <div class="links w-full mt-6 grid grid-cols-2 gap-x-8">
