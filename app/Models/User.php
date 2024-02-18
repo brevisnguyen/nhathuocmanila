2bcbcd659d881b55440ee94690057fc1c72612bb
brevis-ng <brevisnguyen@gmail.com>
2024-02-18 19:06:56 +0800

    feat: add user avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\Role;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -20,6 +21,13 @@ class User extends Authenticatable
         'role' => Role::class
     ];
     protected $attributes = ['role' => Role::GUEST];
+
+    protected function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => 'https://www.gravatar.com/avatar/' . md5($this->email) . '?d=monsterid'
+        );
+    }
 
     public function orders(): HasMany
     {
