3232d8cef20177a5fa962f8cdf1f9caf689273bc
brevis-ng <brevisnguyen@gmail.com>
2024-02-26 19:15:55 +0800

    feat: add relationship and subtotal

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,8 +15,25 @@ class OrderItem extends Model
     protected $fillable = ['order_id', 'product_id', 'unit_id', 'quantity', 'amount'];
     public $timestamps = false;
 
+    protected function subtotal(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->quantity * $this->amount,
+        );
+    }
+
     public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
     }
 }
