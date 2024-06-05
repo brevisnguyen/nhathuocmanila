4b5cfc98a009ac043f6c6215c5390ad06deb1cbb
namndt <tiennam02lory@gmail.com>
2024-06-06 01:31:14 +0800

    vendor: add chart

---
+++ app/Filament/Widgets/OrderRevenueChart.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\Status;
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class OrderRevenueChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+        $data = Trend::query(Order::query()->where('status', Status::COMPLETED))
+            ->between(
+                start: now()->startOfMonth(),
+                end: now()->endOfMonth()
+            )
+            ->perDay()
+            ->sum('subtotal');
+
+        return [
+            'datasets' => [
+                'label' => 'Doanh thu tháng này',
+                'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
